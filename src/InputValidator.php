502b21dabd736e7d7dc4cdf2da63f163702406e3
tdorkaa <tamasdorkaa@gmail.com>
2018-08-31 16:36:23 +0200

    InputValidator refactor

--- src/InputValidator.php
+++ src/InputValidator.php
@@ -9,16 +9,34 @@ class InputValidator
     public function validate($name, $email, $message)
     {
         $errors = [];
-        foreach (['Name' => $name, 'Email' => $email, 'Message' => $message] as $k => $v) {
-            if (!$v && $v !== '0') {
-                $errors[] = $k . ' required';
+        foreach (['Name' => $name, 'Email' => $email, 'Message' => $message] as $key => $value) {
+            if ($this->validateEmpty($value)) {
+                $errors[] = $key . ' required';
             }
-            if($k === 'Email' && $v && !filter_var($v, FILTER_VALIDATE_EMAIL)) {
-                $errors[] = $k . ' is not correct';
+            if($key === 'Email' && $this->validateEmail($value)) {
+                $errors[] = $key . ' is not correct';
             }
         }
         if ($errors) {
             throw new \InvalidArgumentException(implode(',', $errors));
         }
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    private function validateEmpty($value): bool
+    {
+        return !$value && $value !== '0';
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    private function validateEmail($value): bool
+    {
+        return $value && !filter_var($value, FILTER_VALIDATE_EMAIL);
     }
 }
