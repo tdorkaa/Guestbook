3e721c6d9394a262e254588f5dd8790ad0e1f2a8
tdorkaa <tamasdorkaa@gmail.com>
2018-09-04 10:11:13 +0200

    inputfilter trim

--- Tests/InputFilterTest.php
+++ Tests/InputFilterTest.php
@@ -7,13 +7,20 @@ use PHPUnit\Framework\TestCase;
 
 class InputFilterTest extends TestCase
 {
+    private $inputFilter;
+
+    protected function setUp()
+    {
+        $this->inputFilter = new InputFilter();
+    }
+
     /**
      * @test
      */
     public function filter_GivenValueAndWithTag_ReturnsSanitizedValue()
     {
-        $inputFilter = new InputFilter();
-        $value = $inputFilter->filter('<br>');
+
+        $value = $this->inputFilter->filter('<br>');
         $this->assertEquals('', $value);
     }
 
@@ -22,8 +29,7 @@ class InputFilterTest extends TestCase
      */
     public function filter_GivenValueAndNameWithTag_ReturnsSanitizedValue()
     {
-        $inputFilter = new InputFilter();
-        $value = $inputFilter->filter('Test name<br>');
+        $value = $this->inputFilter->filter('Test name<br>');
         $this->assertEquals('Test name', $value);
     }
 
@@ -32,8 +38,25 @@ class InputFilterTest extends TestCase
      */
     public function filter_GivenValueIsValid_ReturnsTheOriginalValue()
     {
-        $inputFilter = new InputFilter();
-        $value = $inputFilter->filter('Test name');
+        $value = $this->inputFilter->filter('Test name');
+        $this->assertEquals('Test name', $value);
+    }
+
+    /**
+     * @test
+     */
+    public function filter_GivenValueIsWitWhiteSpacesAtTheBeginning_ReturnsTheOriginalValue()
+    {
+        $value = $this->inputFilter->filter('                          Test name');
+        $this->assertEquals('Test name', $value);
+    }
+
+    /**
+     * @test
+     */
+    public function filter_GivenValueIsWithNewLineCharacter_ReturnsTheOriginalValue()
+    {
+        $value = $this->inputFilter->filter("Test name\n");
         $this->assertEquals('Test name', $value);
     }
 }
--- src/InputFilter.php
+++ src/InputFilter.php
@@ -7,6 +7,12 @@ class InputFilter
 
     public function filter($value)
     {
+        $value = $this->sanitizeString($value);
+        $value = trim($value);
+        return $value;
+    }
+
+    private function sanitizeString($value) {
         return filter_var($value, FILTER_SANITIZE_STRING);
     }
 }
